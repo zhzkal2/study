25c08e60efb09b0636de5a047b315711485a84db
zhzkal2 <reliya1541@gmail.com>
2024-11-22 02:32:05 +0900

    feat : logback 로그 저장 기능 추가
    fluentd에 보낼 로그 추가

--- softdelete/src/main/java/com/java/UserController.java
+++ softdelete/src/main/java/com/java/UserController.java
@@ -1,11 +1,13 @@
 package com.java;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/users")
 public class UserController {
 
@@ -35,4 +37,16 @@ public class UserController {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/dd")
+    public String dd() {
+
+        log.trace("TRACE!!");
+        log.debug("DEBUG!!");
+        log.info("INFO!!");
+        log.warn("WARN!!");
+        log.error("ERROR!!");
+        return "응애";
+    }
+
 }
