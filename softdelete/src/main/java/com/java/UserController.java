cb91eb1a26cdda25fe9586630c96c96c80da22ae
DESKTOP-4Q5LA11\plo <zhzkal2@naver.com>
2024-11-25 15:51:28 +0900

    feat : cicd 추가

--- softdelete/src/main/java/com/java/UserController.java
+++ softdelete/src/main/java/com/java/UserController.java
@@ -11,33 +11,6 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    // 전체 사용자 조회
-    @GetMapping
-    public ResponseEntity<List<User>> getActiveUsers() {
-        List<User> users = userService.getActiveUsers();
-        return ResponseEntity.ok(users);
-    }
-
-    // 사용자 추가
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody String name) {
-        User newUser = userService.createUser(name);
-        return ResponseEntity.ok(newUser);
-    }
-
-    // 사용자 삭제 (논리 삭제)
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
-    }
-
     @GetMapping("/dd")
     public String dd() {
 
